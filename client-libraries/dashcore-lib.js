504a138c876f7c2b55e956758afb43d7dbb08eb1
Rion Gull <riongull@gmail.com>
2018-09-15 18:47:46 -0600

    added react playground

--- client-libraries/dashcore-lib.js
+++ client-libraries/dashcore-lib.js
@@ -3,7 +3,6 @@ const dashcore = require('@dashevo/dashcore-lib');
 const phrase = new Buffer.from('correct horse battery staple');
 const hash = dashcore.crypto.Hash.sha256(phrase);
 const bn = dashcore.crypto.BN.fromBuffer(hash);
-
 const address = new dashcore.PrivateKey(bn).toAddress();
 
 console.log(`Address for "${phrase}": ${address}`);
